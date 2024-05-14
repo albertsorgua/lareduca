82c9db7c854ff8fe35aab2d76c42ab76934b89f2
Albert <albertsorgua@campus.monlau.com>
2024-05-14 15:59:12 +0200

    update: Realizado el diseño del login del figma en el código

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,17 @@
+<style>
+    .font-macondo {
+        font-family: 'Macondo', cursive;
+    }
+    
+    body {
+        margin: 0;
+        line-height: inherit;
+        background-color: #ECB3B3;
+    }
+</style>
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
-            <x-authentication-card-logo />
         </x-slot>
 
         <x-validation-errors class="mb-4" />
@@ -12,20 +22,24 @@
             </div>
         @endsession
 
-        <form method="POST" action="{{ route('login') }}">
+        <h1 class="font-macondo text-center text-6xl text-black m-10">{{ __('Login') }}</h1>
+
+        <form class="font-macondo" method="POST" action="{{ route('login') }}">
             @csrf
 
-            <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <div class="mb-10">
+                <x-label for="email" value="{{ __('Introduzca el email de la cuenta:') }}" />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                    required autofocus autocomplete="username" />
             </div>
 
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+            <div class="my-4">
+                <x-label for="password" value="{{ __('Introduzca la contraseña de la cuenta:') }}" />
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                    autocomplete="current-password" />
             </div>
 
-            <div class="block mt-4">
+            <div class="block my-4">
                 <label for="remember_me" class="flex items-center">
                     <x-checkbox id="remember_me" name="remember" />
                     <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
@@ -34,7 +48,8 @@
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,4 +1,11 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+<style>
+    .bg-color {
+        margin: 0;
+        line-height: inherit;
+        background-color: #ECB3B3;
+    }
+</style>
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-color">
     <div>
         {{ $logo }}
     </div>
